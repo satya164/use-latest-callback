22f24f2cb3eaa2920bf6ef8d61720ded16f8b7cd
Satyajit Sahoo <satyajit.happy@gmail.com>
2022-06-02 21:16:16 +0200

    fix: don't throw on render until we figure out how to do it correctly

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,11 @@ import React from 'react';
 const useIsomorphicLayoutEffect =
   typeof window !== 'undefined' ? React.useLayoutEffect : React.useEffect;
 
-const throwIfRender = () => {
-  throw new Error(
-    `The callback returned by 'useLatestCallback' should only be used in event listeners and not during render.`
-  );
-};
-
 /**
  * React hook which returns the latest callback without changing the reference.
  */
 export default function useLatestCallback<T extends Function>(callback: T): T {
-  // @ts-expect-error
-  const ref = React.useRef<T>(throwIfRender);
+  const ref = React.useRef<T>(callback);
 
   const latestCallback = React.useRef(function latestCallback(
     this: unknown,
@@ -23,9 +16,6 @@ export default function useLatestCallback<T extends Function>(callback: T): T {
     // eslint-disable-next-line babel/no-invalid-this
     return ref.current.apply(this, args);
   } as unknown as T).current;
-
-  // @ts-expect-error
-  ref.current = throwIfRender;
 
   useIsomorphicLayoutEffect(() => {
     ref.current = callback;
