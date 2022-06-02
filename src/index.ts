9a1fe6729feffbd7cde98591ce06caf80eb51417
Satyajit Sahoo <satyajit.happy@gmail.com>
2022-06-02 13:50:00 +0200

    fix: throw if callback is called during render

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,28 @@ import React from 'react';
 const useIsomorphicLayoutEffect =
   typeof window !== 'undefined' ? React.useLayoutEffect : React.useEffect;
 
+const throwIfRender = () => {
+  throw new Error(
+    `The callback returned by 'useLatestCallback' should only be used in event listeners and not during render.`
+  );
+};
+
 /**
  * React hook which returns the latest callback without changing the reference.
  */
 export default function useLatestCallback<
   T extends (this: unknown, ...args: unknown[]) => unknown
 >(callback: T): T {
-  const ref = React.useRef(callback);
+  // @ts-expect-error
+  const ref = React.useRef<T>(throwIfRender);
 
   const latestCallback = React.useRef(function latestCallback(...args) {
     // eslint-disable-next-line babel/no-invalid-this
     return ref.current.apply(this, args);
   } as T).current;
+
+  // @ts-expect-error
+  ref.current = throwIfRender;
 
   useIsomorphicLayoutEffect(() => {
     ref.current = callback;
