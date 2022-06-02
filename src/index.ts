6de1765490120f94d1ccc96d2ec48910b918e61c
Satyajit Sahoo <satyajit.happy@gmail.com>
2022-06-02 13:42:31 +0200

    refactor: migrate to typescript

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,20 @@
-const React = require('react');
+import React from 'react';
 
 const useIsomorphicLayoutEffect =
   typeof window !== 'undefined' ? React.useLayoutEffect : React.useEffect;
 
 /**
  * React hook which returns the latest callback without changing the reference.
- *
- * @param {T extends (...args: any[]) => void} callback Callback to use as the latest callback
- * @return {T} Memoized callback which preserves the reference
- *
- * @template T
  */
-function useLatestCallback(callback) {
+export default function useLatestCallback<
+  T extends (this: unknown, ...args: unknown[]) => unknown
+>(callback: T): T {
   const ref = React.useRef(callback);
 
   const latestCallback = React.useRef(function latestCallback(...args) {
     // eslint-disable-next-line babel/no-invalid-this
     return ref.current.apply(this, args);
-  }).current;
+  } as T).current;
 
   useIsomorphicLayoutEffect(() => {
     ref.current = callback;
@@ -25,5 +22,3 @@ function useLatestCallback(callback) {
 
   return latestCallback;
 }
-
-module.exports = useLatestCallback;
