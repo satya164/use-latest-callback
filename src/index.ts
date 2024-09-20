f155d8516f199d760806c534c7132fef80f15549
ashwin1014 <ashwin1014@yahoo.co.in>
2024-09-20 12:16:52 +0530

    chore: es-lint fixes

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,15 @@ import useIsomorphicLayoutEffect from './useIsomorphicLayoutEffect';
 /**
  * React hook which returns the latest callback without changing the reference.
  */
-function useLatestCallback<T extends (...args: A) => R, A extends unknown[], R>(callback: T): T {
+function useLatestCallback<T extends (...args: A) => R, A extends unknown[], R>(
+  callback: T
+): T {
   const ref = useRef<T>(callback);
 
-  const latestCallback = useRef(function latestCallback(this: unknown, ...args: A): R {
+  const latestCallback = useRef(function latestCallback(
+    this: unknown,
+    ...args: A
+  ): R {
     return ref.current.apply(this, args);
   } as unknown as T).current;
 
