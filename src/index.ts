9809bf53e61d5a22e76156ecc8ed1f77fadb753e
Satyajit Sahoo <satyajit.happy@gmail.com>
2025-10-11 17:03:48 +0200

    Revert "feat: use `useInsertionEffect` instead of `useLayoutEffect`"
    This reverts commit 4acdb345205716072f93ffa09ae4c629f7e32311.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import * as React from 'react';
 
 /**
+ * Use `useEffect` during SSR and `useLayoutEffect` in the Browser & React Native to avoid warnings.
+ */
+const useClientLayoutEffect =
+  typeof document !== 'undefined' ||
+  (typeof navigator !== 'undefined' && navigator.product === 'ReactNative')
+    ? React.useLayoutEffect
+    : React.useEffect;
+
+/**
  * React hook which returns the latest callback without changing the reference.
  */
 // eslint-disable-next-line @typescript-eslint/ban-types
@@ -14,7 +23,7 @@ function useLatestCallback<T extends Function>(callback: T): T {
     return ref.current.apply(this, args);
   } as unknown as T).current;
 
-  React.useInsertionEffect(() => {
+  useClientLayoutEffect(() => {
     ref.current = callback;
   });
 
